af622efbf4ca8ef63ea462053db0c8044f232d88
Rostislav <suslovros@gmail.com>
2022-01-22 11:37:25 +0200

    layout

--- nimble-shop/src/components/Header/UsersBlock/UsersBlock.js
+++ nimble-shop/src/components/Header/UsersBlock/UsersBlock.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import moduleCSS from './UsersBlock.module.scss';
-import imgUser from '../../../icon-user.svg';
+
 
 const UsersBlock = () => {
     return (
         <div className={moduleCSS.usersBlock}>
-            <img src={imgUser} className={moduleCSS.iconUser} alt="user"/>
+            <div className={moduleCSS.userAvatar}>Р</div>
+            <div className={moduleCSS.userInfo}>
+                <div className={moduleCSS.userName}>
+                    Суслов Ростислав
+                </div>
+                <div className={moduleCSS.userData}>
+                    <div className={moduleCSS.userData__productName}>Дабл Чізбургер</div>
+                    <div className={moduleCSS.userData__salesValue}>5%</div>
+                </div>
+            </div>
+
         </div>
     )
 }
